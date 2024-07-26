60d5597b7dd51219ddad6a2b9d56bee708abfe2d
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2024-07-26 00:04:45 -0300

    Clear DBService

--- src/main/java/com/jcs/helpdesk/services/DBService.java
+++ src/main/java/com/jcs/helpdesk/services/DBService.java
@@ -1,19 +1,11 @@
 package com.jcs.helpdesk.services;
 
-import com.jcs.helpdesk.domain.Associado;
-import com.jcs.helpdesk.domain.Analista;
-import com.jcs.helpdesk.domain.RegistroDeOcorrencia;
-import com.jcs.helpdesk.domain.enums.Perfil;
-import com.jcs.helpdesk.domain.enums.Prioridade;
-import com.jcs.helpdesk.domain.enums.Status;
-import com.jcs.helpdesk.repositories.AssociadoRepository;
 import com.jcs.helpdesk.repositories.AnalistaRepository;
+import com.jcs.helpdesk.repositories.AssociadoRepository;
 import com.jcs.helpdesk.repositories.RegistroDeOcorrenciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
 
 @Service
 public class DBService {
@@ -28,15 +20,6 @@ public class DBService {
     private BCryptPasswordEncoder encoder;
 
     public void instanciaDB() {
-        Analista tec1 = new Analista(null, "Lemes Gustavo", "112233", "Lemesgus@mail.com", encoder.encode("123"));
-        tec1.addPerfil(Perfil.ADMIN);
 
-        Associado cli1 = new Associado(null, "Amanda Fernandes", "261102", "amandafer261102@mail.com", encoder.encode("123"));
-
-        RegistroDeOcorrencia sr1 = new RegistroDeOcorrencia(null, Prioridade.MEDIA, Status.ANDAMENTO, "SR-01", "Primeira RegistroDeOcorrencia", tec1, cli1);
-
-        analistaRepository.saveAll(Arrays.asList(tec1));
-        associadoRepository.saveAll(Arrays.asList(cli1));
-        registroDeOcorrenciaRepository.saveAll(Arrays.asList(sr1));
     }
 }
